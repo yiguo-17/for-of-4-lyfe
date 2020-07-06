2ad2a816e87fe3c636b1a5e75903c9060d5e0dc4
Colin Jaffe <balloonasaurus@gmail.com>
2020-07-06 11:25:32 -0400

    Splits file into solution code and test code files.

---
+++ main.js
@@ -0,0 +1,46 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+
+
+
+
+/*********************************
+ * OUR CODE BELOW; DO NOT TOUCH! *
+ *********************************/
+
+
+
+if (typeof addAll === 'undefined') {
+  addAll = undefined;
+}
+
+if (typeof countSpaces === 'undefined') {
+  countSpaces = undefined;
+}
+
+if (typeof countTrues === 'undefined') {
+  countTrues = undefined;
+}
+
+if (typeof makeFriendly === 'undefined') {
+  makeFriendly = undefined;
+}
+
+if (typeof cubeAll === 'undefined') {
+  cubeAll = undefined;
+}
+
+if (typeof addNoises === 'undefined') {
+  addNoises = undefined;
+}
+
+
+module.exports = {
+  addAll,
+  countSpaces,
+  countTrues,
+  makeFriendly,
+  cubeAll,
+  addNoises,
+}
